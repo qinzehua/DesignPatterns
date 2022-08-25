569018362ce1fe863f8a942ddc2dc403c04b0450
qin.zehua <775935087@qq.com>
2022-08-25 11:42:39 +0800

    组合模式在树形结构中的应用

---
+++ 53/src/index.ts
@@ -0,0 +1,47 @@
+import fs from "fs";
+import path from "path";
+
+class FileSystemNode {
+  list: FileSystemNode[] = [];
+  constructor(private path: string, private isFile: boolean) {}
+  countNumOfFiles(): number {
+    if (this.isFile) return 1;
+    let count = 0;
+    for (const node of this.list) {
+      count += node.countNumOfFiles();
+    }
+    return count;
+  }
+
+  countSizeOfFiles(): number {
+    return 1;
+  }
+
+  getPath(): string {
+    return this.path;
+  }
+
+  addSubNode(fileDir: FileSystemNode): void {
+    this.list.push(fileDir);
+  }
+
+  readFolder() {
+    const subs = fs.readdirSync(this.path);
+    subs.forEach((fileDir) => {
+      const status = fs.statSync(path.join(this.path, fileDir));
+      const node = new FileSystemNode(
+        path.join(this.path, fileDir),
+        status.isFile()
+      );
+      this.addSubNode(node);
+      if (!status.isFile()) {
+        node.readFolder();
+      }
+    });
+  }
+  public removeSubNode(fileDir: FileSystemNode): void {}
+}
+
+const dirSystem = new FileSystemNode(path.join(process.cwd(), "src"), false);
+dirSystem.readFolder();
+console.log(dirSystem.countNumOfFiles());
